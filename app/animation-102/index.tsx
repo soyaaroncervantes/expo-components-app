3d167b7ece76f19358f38b0e398f4f4ca9c1f060
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2025-03-20 11:07:35 -0600

    feat: added animation for drag and drop

--- app/animation-102/index.tsx
+++ app/animation-102/index.tsx
@@ -1,10 +1,34 @@
-import { View, Text } from 'react-native';
+import {useRef} from "react";
+import {Animated, PanResponder, SafeAreaView} from "react-native";
+import {SafeAreaProvider} from "react-native-safe-area-context";
 
 const Animation102Screen = () => {
+  const pan = useRef(new Animated.ValueXY()).current;
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event([
+      null, {
+        dx: pan.x,
+        dy: pan.y,
+      }], {
+      useNativeDriver: false
+    }),
+    onPanResponderRelease: () => {
+      Animated.spring(pan, {
+        toValue: {x: 0, y: 0},
+        useNativeDriver: false
+      }).start();
+    }
+  })
   return (
-    <View>
-      <Text>Animation102Screen</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView className="flex-1 justify-center items-center ">
+        <Animated.View
+          {...panResponder.panHandlers}
+          style={[pan.getLayout()]}
+          className="bg-indigo-400 h-40 w-40 rounded-xl"/>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 export default Animation102Screen;
