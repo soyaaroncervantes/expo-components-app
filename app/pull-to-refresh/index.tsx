97c738227e9eb4f9297c85af8518e7f5b02091f0
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2025-03-21 10:17:10 -0600

    feat: added refreshControl and simple implementation

--- app/pull-to-refresh/index.tsx
+++ app/pull-to-refresh/index.tsx
@@ -1,10 +1,32 @@
-import { View, Text } from 'react-native';
+import {useState} from "react";
+import {RefreshControl, ScrollView} from "react-native";
+import TwColors from "tailwindcss/colors";
+import ThemeView from "@/presentation/theme/View";
+import ThemeText from "@/presentation/theme/Text";
 
 const PullToRefreshScreen = () => {
+  const [isRefreshing, setRefresh] = useState(false);
+  const handleRefresh = async () => {
+    setRefresh(x => !x);
+    setTimeout(() => setRefresh(x => !x), 3000)
+  }
+
   return (
-    <View>
-      <Text>PullToRefreshScreen</Text>
-    </View>
+    <ScrollView
+      contentContainerClassName="flex-1 justify-center items-center"
+      refreshControl={
+        <RefreshControl
+          progressBackgroundColor={TwColors.purple[300]}
+          colors={[TwColors.purple[500]]}
+          refreshing={isRefreshing}
+          onRefresh={handleRefresh}/>
+      }>
+      <ThemeView margin>
+        <ThemeText type='display'>
+          {isRefreshing ? 'Refreshing...' : 'Pull down to refresh'}
+        </ThemeText>
+      </ThemeView>
+    </ScrollView>
   );
 };
 export default PullToRefreshScreen;
